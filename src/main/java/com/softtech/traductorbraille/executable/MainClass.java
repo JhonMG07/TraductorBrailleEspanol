98d5569b6479ee49b62242ad87d45b5d2afe01ec
JexDev13 <jeremy.arias@epn.edu.ec>
2024-05-27 01:55:59 -0500

    Add project enviroment

---
+++ src/main/java/com/softtech/traductorbraille/executable/MainClass.java
@@ -0,0 +1,18 @@
+package com.softtech.traductorbraille.executable;
+
+import com.softtech.traductorbraille.GUI.JFMain;
+
+/*
+ *
+ * @author Isma2
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFMain jfmain = new JFMain();
+        jfmain.setVisible(true);
+    }
+}
