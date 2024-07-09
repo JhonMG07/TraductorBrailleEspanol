a9fcfeedffedca22fc898c86e83ce97913db2856
JexDev13 <jeremy.arias@epn.edu.ec>
2024-07-09 00:33:16 -0500

    Create documentation

--- code/src/main/java/com/softtech/traductorbraille/voice/VoiceService.java
+++ code/src/main/java/com/softtech/traductorbraille/voice/VoiceService.java
@@ -61,8 +61,7 @@ public class VoiceService {
             // Ejecutar el comando
             process = Runtime.getRuntime().exec(cmd);
 
-            try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                 BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
+            try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
 
                 String s;
                 if (outputArea != null) {
@@ -73,7 +72,6 @@ public class VoiceService {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
